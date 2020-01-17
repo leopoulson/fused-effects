0b35c7f06f43bc552146580ea4f6ebfa4d503ae1
Leo Poulson <lp16860@my.bristol.ac.uk>
2020-01-17 17:12:29 +0000

    Add Effect instance

--- src/Control/Effect/Coroutine/Internal.hs
+++ src/Control/Effect/Coroutine/Internal.hs
@@ -14,4 +14,9 @@ data Yield a b (m :: * -> *) c = Yield a (b -> m c)
 -- This is an effect type, so it must implement 'HFunctor' and 'Effect'
 -- TODO: Understand this better
 instance HFunctor (Yield a b) where
+
+  -- hmap :: m x -> n x -> h m a -> h n a
   hmap f (Yield x res) = Yield x (f . res)
+
+instance Effect (Yield a b) where
+  thread ctx handler (Yield x res) = Yield x (handler . (<$ ctx) . res)
